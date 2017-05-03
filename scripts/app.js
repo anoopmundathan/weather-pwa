98b3d73674cffcab3c85c11d4e60c2eeb5488709
anoopmundathan <anoop.mundathan@gmail.com>
2017-05-03 16:30:56 +0100

    Inject weather forecast data

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,33 @@
 
   var weatherAPIUrlBase = 'https://publicdata-weather.firebaseio.com/';
 
+  var injectedForecast = {
+    key: 'newyork',
+    label: 'New York, NY',
+    currently: {
+      time: 1453489481,
+      summary: 'Clear',
+      icon: 'partly-cloudy-day',
+      temperature: 52.74,
+      apparentTemperature: 74.34,
+      precipProbability: 0.20,
+      humidity: 0.77,
+      windBearing: 125,
+      windSpeed: 1.52
+    },
+    daily: {
+      data: [
+        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
+      ]
+    }
+  };
+
   var app = {
     isLoading: true,
     visibleCards: {},
@@ -149,5 +176,7 @@
       app.getForecast(key);
     });
   };
+
+  app.updateForecastCard(injectedForecast);
 
 })();
